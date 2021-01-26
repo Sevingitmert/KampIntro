920673d935779f195612ea23056f2e450ed362e2
mert sevingit <mertsevingit@windowslive.com>
2021-01-26 03:43:47 +0300

    20 Ocak Ders4 Sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>{"emre","aybik","efe","sema"};
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            names.Add("Mert");
+            Console.WriteLine(names[4]);
+            
+        }
+    }
+}
